a642d2df0bb2f8fe25b463b5305b5a3fa12d9d4e
Dennis <djgray780@outlook.com>
2022-01-07 17:14:33 -0800

    adding put and delete methods to the api

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from pydantic import BaseModel
 from typing import Optional
 from random import randrange
 
+from starlette.status import HTTP_204_NO_CONTENT
+
 app = FastAPI()
 
 
 class Post(BaseModel):
+    # The annotation-only declaration tells pydantic that this field is required.
     title: str
     content: str
     published: bool = True  # Defaults to True
@@ -24,6 +27,13 @@ my_posts = [
 def find_post(id: int):
     """Search all posts, return the post on matching index"""
     return [p for p in my_posts if p["id"] == id]
+
+
+def find_index_post(id: int):
+    """Search all posts, return the post"""
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
 
 
 @app.get("/")
@@ -52,4 +62,34 @@ def get_posts(id: int):
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id: {id} was not found.",
         )
+
     return {"post_detail": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    """Delete a post"""
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id:{id} does not exist.",
+        )
+    my_posts.pop(index)
+
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    """Update posts"""
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id:{id} does not exist.",
+        )
+    post_dict = post.dict()
+    post_dict["id"] = id
+    my_posts[index] = post_dict
+    return {"message": post_dict}
