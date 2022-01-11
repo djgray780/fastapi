202260da1c6e627fab19ad1934438254fc17ffc2
Dennis <djgray780@outlook.com>
2022-01-10 18:46:17 -0800

    complete updating SQL statements, clean up.

--- app/main.py
+++ app/main.py
@@ -10,13 +10,14 @@ from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
 
-# Prepare the base Pydantic model 
+# Prepare the base Pydantic model
 class Post(BaseModel):
     # The annotation-only declaration tells pydantic that this field is required.
     title: str
     content: str
     published: bool = True  # Defaults to True
     rating: Optional[int] = None
+
 
 # Establish the connection to the Postgres database.
 try:
@@ -33,24 +34,6 @@ except Exception as error:
     print("Connecting to database failed ...")
     print("Error", error)
 
-# Sample post data.
-my_posts = [
-    {"title": "title of post 1", "content": "content of post 1", "id": 1},
-    {"title": "title of post 2", "content": "content of post 2", "id": 2},
-]
-
-
-def find_post(id: int):
-    """Search all posts, return the post on matching index"""
-    return [p for p in my_posts if p["id"] == id]
-
-
-def find_index_post(id: int):
-    """Search all posts, return the post"""
-    for i, p in enumerate(my_posts):
-        if p["id"] == id:
-            return i
-
 
 # Route definitions
 @app.get("/")
@@ -59,40 +42,70 @@ def root():
 
 
 @app.get("/posts")
-def get_posts():
-    return {"data": my_posts}
+def read_posts():
+    cursor.execute(
+        """ 
+        SELECT * FROM posts 
+        """
+    )
+    posts = cursor.fetchall()
+    print(posts)
+    return {"data": posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):  # variable post of type Post, defined above.
-    post_dict = post.dict()
-    post_dict["id"] = randrange(0, 100000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+def create_posts(post: Post):
+    #
+    cursor.execute(
+        """ 
+        INSERT INTO posts (title, content, published)
+        VALUES (%s, %s, %s)
+        RETURNING *
+        """,
+        (post.title, post.content, post.published),
+    )
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data": new_post}
 
 
 @app.get("/posts/{id}")  # Path parameters are always returned as a string.
 def get_posts(id: int):
-    post = find_post(id)
+    cursor.execute(
+        """ 
+        SELECT * FROM posts
+        WHERE id = %s
+        """,
+        (str(id)),  # Convert id back to string to pass into SQL statement.
+    )
+    post = cursor.fetchone()
+
     if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id: {id} was not found.",
         )
 
-    return {"post_detail": post}
+    return {"data": post}
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    """Delete a post"""
-    index = find_index_post(id)
-    if index == None:
+    """Delete a single post"""
+    cursor.execute(
+        """
+        DELETE FROM posts WHERE id = %s
+        RETURNING *
+     """,
+        (str(id)),
+    )
+    deleted_post = cursor.fetchone()
+    conn.commit()
+    if deleted_post == None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id:{id} does not exist.",
         )
-    my_posts.pop(index)
 
     return Response(status_code=HTTP_204_NO_CONTENT)
 
@@ -100,13 +113,20 @@ def delete_post(id: int):
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
     """Update posts"""
-    index = find_index_post(id)
-    if index == None:
+    cursor.execute(
+        """
+        UPDATE posts 
+        SET title = %s, content = %s, published = %s
+        WHERE id = %s
+        RETURNING *
+        """,
+        (post.title, post.content, post.published, str(id)),
+    )
+    updated_post = cursor.fetchone()
+    conn.commit()
+    if updated_post == None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id:{id} does not exist.",
         )
-    post_dict = post.dict()
-    post_dict["id"] = id
-    my_posts[index] = post_dict
-    return {"message": post_dict}
+    return {"data": updated_post}
